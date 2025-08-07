6df09b23501cbbbab98b5719bac81cfb30603e37
Michael Pereira <michaelpereira408@gmail.com>
2025-08-07 15:09:40 -0300

    feat: update Home page layout with new banners, improved styling, and display of newly created products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,64 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
-export default async function Home() {
+const Home = async () => {
   const products = await db.query.productTable.findMany({
     with: {
       variants: true,
     },
   });
-
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
+    with: {
+      variants: true,
+    },
+  });
   const categories = await db.query.categoryTable.findMany({});
+
   return (
     <>
       <Header />
       <div className="space-y-6">
-        <div>
+        <div className="px-5">
           <Image
             src="/imagens/banner-01.png"
-            alt="Leva a vida com estilo"
+            alt="Leve uma vida com estilo"
             height={0}
             width={0}
             sizes="100vw"
             className="h-auto w-full"
           />
         </div>
+
         <ProductList products={products} title="Mais vendidos" />
-        <div>
+
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
+
+        <div className="px-5">
           <Image
             src="/imagens/banner-02.png"
-            alt="Leva a vida com estilo"
+            alt="Leve uma vida com estilo"
             height={0}
             width={0}
             sizes="100vw"
             className="h-auto w-full"
           />
         </div>
-        <div className="p-5">
-          <CategorySelector categories={categories}/>
-        </div>
+
+        <ProductList products={newlyCreatedProducts} title="Novos produtos" />
+        <Footer />
       </div>
     </>
   );
-}
+};
+
+export default Home;
