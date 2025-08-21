975ee9102c811c096efee6d3635d1c8f83b0528f
Michael Pereira <michaelpereira408@gmail.com>
2025-08-21 00:03:49 -0300

    feat: add Radix UI components for cart functionality and enhance cart item display

--- src/actions/cart/get-cart/index.ts
+++ src/actions/cart/get-cart/index.ts
@@ -1,4 +1,4 @@
-"use server"
+"use server";
 
 import { headers } from "next/headers";
 
@@ -13,19 +13,38 @@ export const getCart = async () => {
   if (!session?.user) {
     throw new Error("Unauthorized");
   }
-
   const cart = await db.query.cartTable.findFirst({
     where: (cart, { eq }) => eq(cart.userId, session.user.id),
-    with: { items: { with: { productVariant: { with: { product: true } } } } }
+    with: {
+      items: {
+        with: {
+          productVariant: {
+            with: {
+              product: true,
+            },
+          },
+        },
+      },
+    },
   });
-
   if (!cart) {
-    const [newCart] = await db.insert(cartTable).values({
-      userId: session.user.id,
-    }).returning();
-
-    return { ...newCart, items: [] };
+    const [newCart] = await db
+      .insert(cartTable)
+      .values({
+        userId: session.user.id,
+      })
+      .returning();
+    return {
+      ...newCart,
+      items: [],
+      totalPriceInCents: 0,
+    };
   }
-
-  return cart;
-}
+  return {
+    ...cart,
+    totalPriceInCents: cart.items.reduce(
+      (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
+      0,
+    ),
+  };
+};
--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,12 +1,22 @@
+"use client";
+
 import { useQuery } from "@tanstack/react-query";
-import { ShoppingBagIcon } from "lucide-react";
-import Image from "next/image";
+import { ShoppingBasketIcon } from "lucide-react";
 
 import { getCart } from "@/actions/cart/get-cart";
 import { formatCentsToBRL } from "@/app/helpers/money";
+import { Button } from "@/components/ui/button";
 
-import { Button } from "../ui/button";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
+import { ScrollArea } from "../ui/scroll-area";
+import { Separator } from "../ui/separator";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import CartItem from "./cart-item";
 
 export const Cart = () => {
   const { data: cart, isPending: cartIsLoading } = useQuery({
@@ -17,31 +27,62 @@ export const Cart = () => {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="outline">
-          <ShoppingBagIcon />
+        <Button variant="outline" size="icon">
+          <ShoppingBasketIcon />
         </Button>
       </SheetTrigger>
       <SheetContent>
-        <SheetHeader><SheetTitle>Meu carrinho</SheetTitle></SheetHeader>
-        <div>
-          {cartIsLoading && <p>Carregando...</p>}
-          {cart?.items.map((item) => (
-            <div key={item.id}>
-              <Image
-                src={item.productVariant.imageUrl.match(/https?:\/\/[^"]+/)?.[0] as string}
-                alt={item.productVariant.product.name}
-                width={50}
-                height={50}
-              />
-              <div>
-                <h3>{item.productVariant.product.name}</h3>
-                <p>Quantidade: {item.quantity}</p>
-                <p>
-                  Preço: {formatCentsToBRL(item.productVariant.priceInCents)}
-                </p>
+        <SheetHeader>
+          <SheetTitle>Carrinho</SheetTitle>
+        </SheetHeader>
+
+        <div className="flex h-full flex-col px-5 pb-5">
+          <div className="flex h-full max-h-full flex-col overflow-hidden">
+            <ScrollArea className="h-full">
+              <div className="flex h-full flex-col gap-8">
+                {cart?.items.map((item) => (
+                  <CartItem
+                    key={item.id}
+                    id={item.id}
+                    productName={item.productVariant.product.name}
+                    productVariantName={item.productVariant.name}
+                    productVariantImageUrl={item.productVariant.imageUrl}
+                    productVariantPriceInCents={
+                      item.productVariant.priceInCents
+                    }
+                    quantity={item.quantity}
+                  />
+                ))}
               </div>
+            </ScrollArea>
+          </div>
+
+          {cart?.items && cart?.items.length > 0 && (
+            <div className="flex flex-col gap-4">
+              <Separator />
+
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Subtotal</p>
+                <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
+              </div>
+
+              <Separator />
+
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Entrega</p>
+                <p>GRÁTIS</p>
+              </div>
+
+              <Separator />
+
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Total</p>
+                <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
+              </div>
+
+              <Button className="mt-5 rounded-full">Finalizar compra</Button>
             </div>
-          ))}
+          )}
         </div>
       </SheetContent>
     </Sheet>
